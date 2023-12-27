9c96c0eb26fbf874a4c03b2fd04bd95f586e0310
gibaoild <81472928+gibaoild@users.noreply.github.com>
2023-12-27 17:03:04 +0700

    convolutionKernel

--- src/layer/conv_gpu.cu
+++ src/layer/conv_gpu.cu
@@ -26,3 +26,41 @@ void Conv::im2col(const Vector& image, Matrix& data_col) {
     // Copy results back to CPU
     // Free GPU memory
 }
+
+__global__ void GPUConvolutionKernel(uchar3 * inPixels, int width, int height, 
+		float * filter, int filterWidth, 
+		uchar3 * outPixels)
+{
+	// TODO
+	int r = blockIdx.x * blockDim.x + threadIdx.x;
+	int c = blockIdx.y * blockDim.y + threadIdx.y;
+	int padding = filterWidth / 2;
+	int i = r * width + c;
+
+	if (r < width && c < height){
+		float red = 0.0f;
+		float green = 0.0f;
+		float blue = 0.0f;
+		for (int y = -padding; y <= padding; y++) {
+            for (int x = -padding; x <= padding; x++) {
+				int currentX = x + c;
+				int currentY = y + r;
+
+				currentX = (currentX < 0) ? 0 : currentX;
+				currentX = (currentX > (width - 1)) ? (width - 1) : currentX;
+				currentY = (currentY < 0) ? 0 : currentY;
+				currentY = (currentY > (height - 1)) ? (height - 1) : currentY;
+						
+				int filterIdx = (y + padding) * filterWidth + x + padding;
+				int pixelIdx = currentY * width + currentX;
+
+				red += filter[filterIdx] * inPixels[pixelIdx].x;
+				green += filter[filterIdx] * inPixels[pixelIdx].y;
+				blue += filter[filterIdx] * inPixels[pixelIdx].z;
+        	}
+        }
+		outPixels[i].x = red;
+		outPixels[i].y = green;
+		outPixels[i].z = blue;
+	}
+}
