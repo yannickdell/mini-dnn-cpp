19b71f36b94155ddc5759797c8611b32d2267543
gibaoild <81472928+gibaoild@users.noreply.github.com>
2023-12-30 19:40:31 +0700

    remove conv.h, add streams to overlap data transfer

--- gpu_layer/gpu_conv.cu
+++ gpu_layer/gpu_conv.cu
@@ -1,7 +1,33 @@
 #include "../../src/layer/conv.h"
 
-#define TILE_WIDTH 16
+#define TILE_WIDTH 32
 
+#define CHECK(call)\
+{\
+    const cudaError_t error = call;\
+    if (error != cudaSuccess)\
+    {\
+        fprintf(stderr, "Error: %s:%d, ", __FILE__, __LINE__);\
+        fprintf(stderr, "code: %d, reason: %s\n", error,\
+                cudaGetErrorString(error));\
+        exit(EXIT_FAILURE);\
+    }\
+}
+// v1_use global memory
+__global__ void matMulKernel_v1(float* A, float* B, float* C, int M, int N, int P) {
+  int row = blockIdx.y * blockDim.y + threadIdx.y;
+  int col = blockIdx.x * blockDim.x + threadIdx.x;
+
+  if (row < M && col < P) {
+    float sum = 0;
+    for (int i = 0; i < N; i++) {
+      sum += A[row * N + i] * B[i * P + col];
+    }
+    C[row * P + col] = sum;
+  }
+}
+
+// v2_use shared memory
 __global__ void matMulKernel_v2(float* A, float* B, float* C, int M, int N, int P) {
   // Allocate shared memory
   __shared__ float As[TILE_WIDTH][TILE_WIDTH];
@@ -13,7 +39,7 @@ __global__ void matMulKernel_v2(float* A, float* B, float* C, int M, int N, int 
   float sum = 0;
 
   // Loop over the tiles of the input matrices
-  for (int m = 0; m < (N - 1)/TILE_WIDTH + 1; ++m) {
+  for (int m = 0; m < ((N - 1)/TILE_WIDTH + 1); ++m) {
 
     // Load one tile of A and B into shared memory
     if (row < M && m*TILE_WIDTH + threadIdx.x < N)
@@ -42,157 +68,126 @@ __global__ void matMulKernel_v2(float* A, float* B, float* C, int M, int N, int 
     C[row * P + col] = sum;
 }
 
-__global__ void im2colKernel(const float* image, float* data_col, int height_in, int width_in, int height_out, int width_out, int height_kernel, int width_kernel, int stride, int pad_h, int pad_w) {
-  int i = blockIdx.x * blockDim.x + threadIdx.x;
+// __global__ void im2colKernel(const float* image, float* data_col, int height_in, int width_in, int height_out, int width_out, int height_kernel, int width_kernel, int stride, int pad_h, int pad_w) {
+//   int i = blockIdx.x * blockDim.x + threadIdx.x;
 
-  if (i < height_out * width_out) {
-    int step_h = i / width_out;
-    int step_w = i % width_out;
-    int start_idx = step_h * width_in * stride + step_w * stride;  // left-top idx of window
+//   if (i < height_out * width_out) {
+//     int step_h = i / width_out;
+//     int step_w = i % width_out;
+//     int start_idx = step_h * width_in * stride + step_w * stride;  // left-top idx of window
 
-    for (int j = 0; j < height_kernel * width_kernel; j ++) {
-      int cur_col = start_idx % width_in + j % width_kernel - pad_w;  // col after padding
-      int cur_row = start_idx / width_in + j / width_kernel - pad_h;
+//     for (int j = 0; j < height_kernel * width_kernel; j ++) {
+//       int cur_col = start_idx % width_in + j % width_kernel - pad_w;  // col after padding
+//       int cur_row = start_idx / width_in + j / width_kernel - pad_h;
 
-      if (cur_col < 0 || cur_col >= width_in || cur_row < 0 || cur_row >= height_in) {
-        data_col[i * height_kernel * width_kernel + j] = 0;
-      }
-      else {
-        int pick_idx = cur_row * width_in + cur_col;
-        data_col[i * height_kernel * width_kernel + j] = image[pick_idx];  // pick which pixel
-      }
-    }
-  }
-}
+//       if (cur_col < 0 || cur_col >= width_in || cur_row < 0 || cur_row >= height_in) {
+//         data_col[i * height_kernel * width_kernel + j] = 0;
+//       }
+//       else {
+//         int pick_idx = cur_row * width_in + cur_col;
+//         data_col[i * height_kernel * width_kernel + j] = image[pick_idx];  // pick which pixel
+//       }
+//     }
+//   }
+// }
 
-void Conv::im2col(const Vector& image, Matrix& data_col) {
-  // Convert Eigen Vector to array
-  const float* image_array = image.data();
+// void Conv::im2col(const Vector& image, Matrix& data_col) {
+//   // Convert Eigen Vector to array
+//   const float* image_array = image.data();
 
-  // Calculate dimensions
-  int hw_in = height_in * width_in;
-  int hw_kernel = height_kernel * width_kernel;
-  int hw_out = height_out * width_out;
+//   // Calculate dimensions
+//   int hw_in = height_in * width_in;
+//   int hw_kernel = height_kernel * width_kernel;
+//   int hw_out = height_out * width_out;
 
-  // Allocate GPU memory
-  float* d_image;
-  float* d_data_col;
-  cudaMalloc(&d_image, hw_in * sizeof(float));
-  cudaMalloc(&d_data_col, hw_out * hw_kernel * sizeof(float));
+//   // Allocate GPU memory
+//   float* d_image;
+//   float* d_data_col;
+//   cudaMalloc(&d_image, hw_in * sizeof(float));
+//   cudaMalloc(&d_data_col, hw_out * hw_kernel * sizeof(float));
 
-  // Copy data to GPU
-  cudaMemcpy(d_image, image_array, hw_in * sizeof(float), cudaMemcpyHostToDevice);
+//   // Copy data to GPU
+//   cudaMemcpy(d_image, image_array, hw_in * sizeof(float), cudaMemcpyHostToDevice);
 
-  // Call GPU kernel
-  int numThreads = 256;
-  int numBlocks = (hw_out + numThreads - 1) / numThreads;
-  im2colKernel<<<numBlocks, numThreads>>>(d_image, d_data_col, height_in, width_in, height_out, width_out, height_kernel, width_kernel, stride, pad_h, pad_w);
-  cudaDeviceSynchronize();
+//   // Call GPU kernel
+//   int numThreads = 256;
+//   int numBlocks = (hw_out + numThreads - 1) / numThreads;
 
-  // Copy result back to CPU
-  float* data_col_array = new float[hw_out * hw_kernel];
-  cudaMemcpy(data_col_array, d_data_col, hw_out * hw_kernel * sizeof(float), cudaMemcpyDeviceToHost);
+//   im2colKernel<<<numBlocks, numThreads,smem_size>>>(d_image, d_data_col, height_in, width_in, height_out, width_out, height_kernel, width_kernel, stride, pad_h, pad_w);
+//   cudaDeviceSynchronize();
 
-  // Convert result array back to Eigen matrix
-  data_col = Eigen::Map<Matrix>(data_col_array, hw_out, hw_kernel);
+//   // Copy result back to CPU
+//   float* data_col_array = new float[hw_out * hw_kernel];
+//   cudaMemcpy(data_col_array, d_data_col, hw_out * hw_kernel * sizeof(float), cudaMemcpyDeviceToHost);
 
-  // Free GPU memory
-  cudaFree(d_image);
-  cudaFree(d_data_col);
+//   // Convert result array back to Eigen matrix
+//   data_col = Eigen::Map<Matrix>(data_col_array, hw_out, hw_kernel);
 
-  // Delete result array
-  delete[] data_col_array;
-}
+//   // Free GPU memory
+//   cudaFree(d_image);
+//   cudaFree(d_data_col);
 
+//   // Delete result array
+//   delete[] data_col_array;
+// }
+
+// v3_ use stream
 void Conv::forward(const Matrix& bottom){
-    int n_sample = bottom.cols();
-    top.resize(height_out * width_out * channel_out, n_sample);
-    data_cols.resize(n_sample);
-    for (int i = 0; i < n_sample; i ++) {
-        // im2col
-        Matrix data_col;
-        im2col(bottom.col(i), data_col);
-        data_cols[i] = data_col;
-        // conv by product
+  int n_sample = bottom.cols();
+  top.resize(height_out * width_out * channel_out, n_sample);
+  data_cols.resize(n_sample);
 
-        //Matrix result = data_col * weight;  // result: (hw_out, channel_out)
-        float* data_col_array = data_col.data();
-        float* weight_array = weight.data();
-        float* result_array = new float[data_col.rows() * weight.cols()];
+  cudaStream_t stream1, stream2;
+  CHECK(cudaStreamCreate(&stream1));
+  CHECK(cudaStreamCreate(&stream2));
 
-        // Allocate GPU memory
-        float* d_data_col;
-        float* d_weight;
-        float* d_result;
-        cudaMalloc(&d_data_col, data_col.size() * sizeof(float));
-        cudaMalloc(&d_weight, weight.size() * sizeof(float));
-        cudaMalloc(&d_result, data_col.rows() * weight.cols() * sizeof(float));
+  for (int i = 0; i < n_sample; i ++) {
+    // im2col
+    Matrix data_col;
+    im2col(bottom.col(i), data_col);
+    data_cols[i] = data_col;
 
-        // Copy data to GPU
-        cudaMemcpy(d_data_col, data_col_array, data_col.size() * sizeof(float), cudaMemcpyHostToDevice);
-        cudaMemcpy(d_weight, weight_array, weight.size() * sizeof(float), cudaMemcpyHostToDevice);
+    float* data_col_array = data_col.data();
+    float* weight_array = weight.data();
+    float* result_array = new float[data_col.rows() * weight.cols()];
 
-        // Call GPU kernel
-        dim3 threadsPerBlock(16, 16);
-        dim3 numBlocks((weight.cols() + threadsPerBlock.x - 1) / threadsPerBlock.x, (data_col.rows() + threadsPerBlock.y - 1) / threadsPerBlock.y);
-        matMulKernel<<<numBlocks, threadsPerBlock>>>(d_data_col, d_weight, d_result, data_col.rows(), data_col.cols(), weight.cols());
-        cudaDeviceSynchronize();
+    // Allocate GPU memory
+    float* d_data_col;
+    float* d_weight;
+    float* d_result;
+    CHECK(cudaMalloc(&d_data_col, data_col.size() * sizeof(float)));
+    CHECK(cudaMalloc(&d_weight, weight.size() * sizeof(float)));
+    CHECK(cudaMalloc(&d_result, data_col.rows() * weight.cols() * sizeof(float)));
 
-        // Copy result back to CPU
-        cudaMemcpy(result_array, d_result, data_col.rows() * weight.cols() * sizeof(float), cudaMemcpyDeviceToHost);
+    // Copy data to GPU
+    CHECK(cudaMemcpyAsync(d_data_col, data_col_array, data_col.size() * sizeof(float), cudaMemcpyHostToDevice, stream1));
+    CHECK(cudaMemcpyAsync(d_weight, weight_array, weight.size() * sizeof(float), cudaMemcpyHostToDevice, stream2));
 
-        // Convert result array back to Eigen matrix
-        Matrix result = Eigen::Map<Matrix>(result_array, data_col.rows(), weight.cols());
+    // Call GPU kernel
+    dim3 threadsPerBlock(32, 32);
+    dim3 numBlocks((weight.cols() + threadsPerBlock.x - 1) / threadsPerBlock.x, (data_col.rows() + threadsPerBlock.y - 1) / threadsPerBlock.y);
+    int smem_size = (threadsPerBlock.x + TILE_WIDTH - 1)*(threadsPerBlock.y + TILE_WIDTH - 1) * sizeof(float);
+    matMulKernel_v2<<<numBlocks, threadsPerBlock, smem_size>>>(d_data_col, d_weight, d_result, data_col.rows(), data_col.cols(), weight.cols());
+    cudaDeviceSynchronize();
 
-        result.rowwise() += bias.transpose();
-        top.col(i) = Eigen::Map<Vector>(result.data(), result.size());
+    // Copy result back to CPU
+    CHECK(cudaMemcpy(result_array, d_result, data_col.rows() * weight.cols() * sizeof(float), cudaMemcpyDeviceToHost));
 
-        // Free GPU memory
-        cudaFree(d_data_col);
-        cudaFree(d_weight);
-        cudaFree(d_result);
+    // Convert result array back to Eigen matrix
+    Matrix result = Eigen::Map<Matrix>(result_array, data_col.rows(), weight.cols());
 
-        // Delete result array
-        delete[] result_array;
-    }
-}
+    result.rowwise() += bias.transpose();
+    top.col(i) = Eigen::Map<Vector>(result.data(), result.size());
 
-__global__ void ConvKernel_v1(int * in, int width, int height, 
-    float * kernel, int kernel_width, int * out) {
-    int r = blockIdx.x * blockDim.x + threadIdx.x;
-    int c = blockIdx.y * blockDim.y + threadIdx.y;
-    int padding = kernel_width / 2;
-    int i = r * width + c;
+    // Free GPU memory
+    CHECK(cudaFree(d_data_col));
+    CHECK(cudaFree(d_weight));
+    CHECK(cudaFree(d_result));
 
-    if (r < width && c < height){
-        for (int y = -padding; y <= padding; y++) {
-            for (int x = -padding; x <= padding; x++) {
-                int currentX = x + c;
-                int currentY = y + r;
-
-                currentX = (currentX < 0) ? 0 : currentX;
-                currentX = (currentX > (width - 1)) ? (width - 1) : currentX;
-                currentY = (currentY < 0) ? 0 : currentY;
-                currentY = (currentY > (height - 1)) ? (height - 1) : currentY;
-                        
-                int filterIdx = (y + padding) * kernel_width + x + padding;
-                int pixelIdx = currentY * width + currentX;
-
-                out[i] += kernel[filterIdx] * in[pixelIdx];
-            }
-        }
-    }
-}
-
-global void matMulKernel_v1(float* A, float* B, float* C, int M, int N, int P) {
-    int row = blockIdx.y * blockDim.y + threadIdx.y;
-    int col = blockIdx.x * blockDim.x + threadIdx.x;
-  
-    if (row < M && col < P) {
-      float sum = 0;
-      for (int i = 0; i < N; i++) {
-        sum += A[row * N + i] * B[i * P + col];
-      }
-      C[row * P + col] = sum;
-    }
+    // Delete result array
+    delete[] result_array;
   }
+
+  CHECK(cudaStreamDestroy(stream1));
+  CHECK(cudaStreamDestroy(stream2));
+}
--- gpu_layer/gpu_conv.h
+++
@@ -1,5 +0,0 @@
-// #ifndef GPU_LAYER_CONV_H_
-// #define GPU_LAYER_CONV_H_
-// #include "../src/layer/conv.h"
-
-// #endif  // GPU_LAYER_CONV_H_
